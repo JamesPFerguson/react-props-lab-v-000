585d3c5f314f2456cd3da951383dce3ca9733144
James Ferguson <ncjamesferguson@gmail.com>
2018-07-08 21:44:49 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,5 +1,5 @@
-// Code The Spaceship Component Here
-import "React" from "react"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 class Spaceship extends React.component {
   render() {
